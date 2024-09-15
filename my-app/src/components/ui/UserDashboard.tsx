bf9224ade9e9fd2b79d7a28adecae4bd5e8ca3ae
Marmik Patel <marmikp19@gmail.com>
2024-09-15 10:38:44 -0400

    fix stat ui

--- my-app/src/components/ui/UserDashboard.tsx
+++ my-app/src/components/ui/UserDashboard.tsx
@@ -12,6 +12,10 @@ import {
 import EmsCallContainer from "./EmsCallContainer";
 import colors from "../../colors";
 import { CiCalendar } from "react-icons/ci";
+import * as SeparatorVer from '@radix-ui/react-separator';
+import { FaRegThumbsUp } from "react-icons/fa";
+import { MdOutlineTimer } from "react-icons/md";
+import { FaChartLine } from "react-icons/fa6";
 
 // Props for the user data, stats, and tasks
 type DashboardProps = {
@@ -71,30 +75,55 @@ function UserDashboard({
 
         <Separator orientation="horizontal" size="4" mt="20px" />
         {/* Stats Section */}
-        <Flex justify="between" align="center" mb="20px">
-          <Box>
-            <Text size="5" weight="bold" style={{ color: "white" }}>
-              Finished
-            </Text>
-            <Text size="4" style={{ color: "#BB86FC" }}>
-              {stats.finished} <span>+{stats.finishedTasks} tasks</span>
-            </Text>
+        <Flex justify="between" align="center" mb="20px" style={{ marginTop: "25px" }}>
+          <Box style={{ display: 'flex', alignItems: 'center', marginLeft: "50px"}}>
+            <FaRegThumbsUp style={{ fontSize: "1.4rem", marginRight: "10px"}} />
+            <Box style={{ display: 'flex', flexDirection: 'column' }}>
+              <Text size="5" weight="bold" style={{ color: "white" }}>
+                Finished
+              </Text>
+              <Text size="4" style={{ color: "#BB86FC" }}>
+                {stats.finished} <span>+{stats.finishedTasks} tasks</span>
+              </Text>
+            </Box>
           </Box>
-          <Box>
-            <Text size="5" weight="bold" style={{ color: "white" }}>
-              Tracked
-            </Text>
-            <Text size="4" style={{ color: "#BB86FC" }}>
-              {stats.tracked}h <span>{stats.trackedHoursChange} hours</span>
-            </Text>
+
+          <SeparatorVer.Root
+            className="SeparatorRoot"
+            decorative
+            orientation="vertical"
+            style={{ borderColor: 'white', borderStyle: 'solid', borderWidth: '1px', height: '45px' }}
+          />
+
+          <Box style={{ display: 'flex', alignItems: 'center' }}>
+            <MdOutlineTimer style={{ fontSize: "1.8rem", marginRight: "10px"}}/>
+            <Box style={{ display: 'flex', flexDirection: 'column' }}>
+              <Text size="5" weight="bold" style={{ color: "white" }}>
+                Tracked
+              </Text>
+              <Text size="4" style={{ color: "#BB86FC" }}>
+                {stats.tracked}h <span>{stats.trackedHoursChange} hours</span>
+              </Text>
+            </Box>
           </Box>
-          <Box>
-            <Text size="5" weight="bold" style={{ color: "white" }}>
-              Efficiency
-            </Text>
-            <Text size="4" style={{ color: "#BB86FC" }}>
-              {stats.efficiency}% <span>{stats.efficiencyChange}</span>
-            </Text>
+
+          <SeparatorVer.Root
+            className="SeparatorRoot"
+            decorative
+            orientation="vertical"
+            style={{ borderColor: 'white', borderStyle: 'solid', borderWidth: '1px', height: '45px' }}
+          />
+
+          <Box style={{ display: 'flex', alignItems: 'center', marginRight: "50px" }}>
+            <FaChartLine  style={{ fontSize: "1.6rem", marginRight: "10px"}}/>
+            <Box style={{ display: 'flex', flexDirection: 'column' }}>
+              <Text size="5" weight="bold" style={{ color: "white" }}>
+                Efficiency
+              </Text>
+              <Text size="4" style={{ color: "#BB86FC" }}>
+                {stats.efficiency}% <span>{stats.efficiencyChange}</span>
+              </Text>
+            </Box>
           </Box>
         </Flex>
         <Separator orientation="horizontal" size="4" mt="20px" mb="20px" />
