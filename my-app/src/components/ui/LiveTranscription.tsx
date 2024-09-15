a063f39d31ff0a090c812d8da4b6abe3f85da081
Danny Xu <danny.d.xu@gmail.com>
2024-09-15 09:55:10 -0400

    fixed convex 10006 bug

--- my-app/src/components/ui/LiveTranscription.tsx
+++ my-app/src/components/ui/LiveTranscription.tsx
@@ -69,79 +69,37 @@ function LiveTranscription({ setSelectedConversation }: Props) {
                   paddingRight: "10px",
                 }}
               >
-                {conversation.messages.map((message, index) => (
-                  <Flex
-                    key={index}
-                    justify={message.messageUser === "caller" ? "start" : "end"} // Using messageUser to align
-                    mb="10px"
-                  >
-                    <Card
-                      style={{
-                        maxWidth: "60%",
-                        backgroundColor: "#FFFFFF09",
-                        color: "white",
-                        borderRadius: "12px",
-                        padding: "12px",
-                      }}
+                {conversation?.messages?.length > 0 ? (
+                  conversation.messages.map((message, index) => (
+                    <Flex
+                      key={index}
+                      justify={
+                        message.messageUser === "caller" ? "start" : "end"
+                      } // Using messageUser to align
+                      mb="10px"
                     >
-                      <Text size="3" as="div">
-                        {message.messageContent} {/* Show message content */}
-                      </Text>
-                    </Card>
-                  </Flex>
-                ))}
+                      <Card
+                        style={{
+                          maxWidth: "60%",
+                          backgroundColor: "#FFFFFF09",
+                          color: "white",
+                          borderRadius: "12px",
+                          padding: "12px",
+                        }}
+                      >
+                        <Text size="3" as="div">
+                          {message.messageContent || "No content"}{" "}
+                          {/* Show message content */}
+                        </Text>
+                      </Card>
+                    </Flex>
+                  ))
+                ) : (
+                  <Text>No messages available</Text>
+                )}
                 <div ref={messagesEndRef} />
               </Box>
             </Card>
-
-            <Separator my="20px" />
-
-            <Card mb="10px">
-              <Flex justify="center">
-                <Box>
-                  <Text size="4" weight="bold" as="div">
-                    {conversation.summary || "No summary yet"}
-                  </Text>
-                </Box>
-              </Flex>
-            </Card>
-
-            <Flex justify="center" width="100%" gap="20px">
-              <Card mb="10px">
-                <Flex justify="center">
-                  <Text size="4" as="div" mr="20px">
-                    {conversation.urgency && `Urgency: ${conversation.urgency}`}
-                  </Text>
-                </Flex>
-              </Card>
-              <Card mb="10px">
-                <Flex justify="center">
-                  <Text size="4" as="div">
-                    {conversation.dispatchConnected &&
-                      `Dispatch Connected: ${conversation.dispatchConnected}`}
-                  </Text>
-                </Flex>
-              </Card>
-            </Flex>
-
-            <Flex justify="center" width="100%" gap="20px">
-              <Card mb="10px">
-                <Flex justify="center">
-                  <Text size="4" as="div" mr="20px">
-                    {conversation.EMSName &&
-                      `EMS Name: ${conversation.EMSName}`}
-                  </Text>
-                </Flex>
-              </Card>
-              <Card mb="10px">
-                <Flex justify="center">
-                  <Text size="4" as="div">
-                    {conversation.medicalEmergencyType &&
-                      `Emergency Type: ${conversation.medicalEmergencyType}`}
-                  </Text>
-                </Flex>
-              </Card>
-            </Flex>
           </Box>
         </Card>
       </Box>
